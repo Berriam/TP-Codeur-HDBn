7e8a6c7b70e84031973d1e06f15bb7a524569c5d
William Berard <berardwilliam@gmail.com>
2017-02-01 10:01:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int NBITS = 24;
+
+void initZero(int tab[], int taille){
+    int i;
+    for(i = 0; i < taille; i++){
+        tab[i] = 0; 
+    }
+}
+
+void dataBis(int Databis[], int P[], int N[]){
+    int i;
+    for(i = 0; i < NBITS; i++){
+        switch(Databis[i]{
+            case 1: P[i] = 1; break;
+            case -1: N[i] = 1; break;
+            case 0: break;
+            default: printf("erreur, valeur %i de dataBis invalide", i); 
+        }
+    }
+}
+               
+void afficher(
+               
 void main(){
-  int Data[24]={1,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0};  
+    int v = -1;
+    int P[NBITS];
+    int N[NBITS];
+    int dataBis[NBITS];
+    initZero(N, NBITS);
+    initZero(P, NBITS);
+    int Data[NBITS]={1,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0}; 
+    v = convertTab(Data, Databis, v);
+    dataBis(Databis, P, N);
 }
