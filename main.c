e26a47e0c6862e6a961716d22ed3a9d50aad6bfa
William Berard <berardwilliam@gmail.com>
2017-02-01 10:39:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ void dataBis(int Databis[], int P[], int N[]){
     	int i;
     	for(i = 0; i < NBITS; i++){
         	switch(Databis[i]){
-           	 case 1: P[i] = 1; break;
+           	case 1: P[i] = 1; break;
             	case -1: N[i] = 1; break;
             	case 0: break;
             	default: printf("erreur, valeur %i de dataBis invalide", i); 
@@ -22,6 +22,7 @@ void dataBis(int Databis[], int P[], int N[]){
    	 }
 }
 int tripleZero(int Databis[], int v, int n){
+	printf("\n\n\n%i\n\n\n", v);
 	// cas ou V est positif	
 	if(v==1){
 		if (Databis[n-3]==1){
@@ -49,12 +50,18 @@ int tripleZero(int Databis[], int v, int n){
 void convertTab(int Data[], int Databis[], int v){
 	int n,comptZeros=0,neg=1;
 	for(n=0;n<NBITS;n++){
-		if(Data[n]==0) comptZeros++;
+		if(Data[n]==0){
+			 comptZeros++;
+			 Databis[n] = 0;
+		}
+		
 		if(comptZeros==3){
+			
 			v=tripleZero(Databis,v,n);
 			comptZeros=0;
 		}
 		else if(Data[n]!=0){
+			comptZeros = 0;
 			if(neg==1)Databis[n]=Data[n]; 
 			else if(neg==-1)Databis[n]=-Data[n];
 			neg=-neg;
@@ -64,9 +71,11 @@ void convertTab(int Data[], int Databis[], int v){
 
 void afficher(int Databis[]){
 	int i;
+	printf("\n");
 	for(i = 0; i < NBITS; i++){
-		printf("%i", Databis[i]);
+		printf("%i ", Databis[i]);
 	}
+	printf("\n");
 }
                
 void main(){
@@ -77,7 +86,8 @@ void main(){
     	initZero(N, NBITS);
     	initZero(P, NBITS);
     	int Data[NBITS]={1,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0}; 
-    	v = convertTab(Data, Databis, v);
+    	convertTab(Data, Databis, v);
     	dataBis(Databis, P, N);
+    	afficher(Data);
     	afficher(Databis);
 }
