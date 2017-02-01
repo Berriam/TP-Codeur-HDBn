127d7b865e1fd963864927ba947cbd9a7586c57a
William Berard <berardwilliam@gmail.com>
2017-02-01 10:10:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,24 +4,24 @@
 #define NBITS 24
 
 void initZero(int tab[], int taille){
-    int i;
-    for(i = 0; i < taille; i++){
-        tab[i] = 0; 
-    }
+    	int i;
+    	for(i = 0; i < taille; i++){
+     	   tab[i] = 0; 
+    	}
 }
 
 void dataBis(int Databis[], int P[], int N[]){
-    int i;
-    for(i = 0; i < NBITS; i++){
-        switch(Databis[i]{
-            case 1: P[i] = 1; break;
-            case -1: N[i] = 1; break;
-            case 0: break;
-            default: printf("erreur, valeur %i de dataBis invalide", i); 
-        }
-    }
+    	int i;
+    	for(i = 0; i < NBITS; i++){
+        	switch(Databis[i]){
+           	 case 1: P[i] = 1; break;
+            	case -1: N[i] = 1; break;
+            	case 0: break;
+            	default: printf("erreur, valeur %i de dataBis invalide", i); 
+        	}
+   	 }
 }
-int tripleZero(int Data, int v, int n){
+int tripleZero(int Data[], int v, int n){
 	// cas ou V est positif	
 	if(v==1){
 		if (Data[n-3]==1){
@@ -46,27 +46,35 @@ int tripleZero(int Data, int v, int n){
 	return -v;
 } 
 
-int convertTab(int Data,Databis,v){
+int convertTab(int Data[], int Databis[], int v){
 	int n,comptZeros=0;
 	for(n=0;n<NBITS;n++){
 		if(Data[n]==0) comptZeros++;
 		if(comptZeros==3){
-			v=tripleZeros(Databis[],v,n);
+			v=tripleZero(Databis,v,n);
 			comptZeros=0;
 		}
 		else Databis[n]=Data[n]; 
 	}
 	return v;
 }
+
+void afficher(int Databis[]){
+	int i;
+	for(i = 0; i < NBITS; i++){
+		printf("%i", Databis[i]);
+	}
+}
                
 void main(){
-    int v = -1;
-    int P[NBITS];
-    int N[NBITS];
-    int DataBis[NBITS];
-    initZero(N, NBITS);
-    initZero(P, NBITS);
-    int Data[NBITS]={1,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0}; 
-    v = convertTab(Data, Databis, v);
-    dataBis(Databis, P, N);
+	int v = -1;
+    	int P[NBITS];
+    	int N[NBITS];
+    	int Databis[NBITS];
+    	initZero(N, NBITS);
+    	initZero(P, NBITS);
+    	int Data[NBITS]={1,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0}; 
+    	v = convertTab(Data, Databis, v);
+    	dataBis(Databis, P, N);
+    	afficher(Databis);
 }
